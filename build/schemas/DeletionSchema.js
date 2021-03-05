a8cde471e393f9dc196cc57685ead63d981bb269
yaboyaxis <54381371+yaboyaxis@users.noreply.github.com>
2021-03-05 15:38:00 -0500

    refactor: Delete and ignore build folder

--- build/schemas/DeletionSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const joi_1 = require("joi");
-exports.default = joi_1.object({
-    key: joi_1.string()
-        .required(),
-}).options({ abortEarly: false });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRGVsZXRpb25TY2hlbWEuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvc2NoZW1hcy9EZWxldGlvblNjaGVtYS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDZCQUFxQztBQUVyQyxrQkFBZSxZQUFNLENBQUM7SUFDbEIsR0FBRyxFQUFFLFlBQU0sRUFBRTtTQUNSLFFBQVEsRUFBRTtDQUNsQixDQUFDLENBQUMsT0FBTyxDQUFDLEVBQUUsVUFBVSxFQUFFLEtBQUssRUFBRSxDQUFDLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBvYmplY3QsIHN0cmluZyB9IGZyb20gJ2pvaSc7XHJcblxyXG5leHBvcnQgZGVmYXVsdCBvYmplY3Qoe1xyXG4gICAga2V5OiBzdHJpbmcoKVxyXG4gICAgICAgIC5yZXF1aXJlZCgpLFxyXG59KS5vcHRpb25zKHsgYWJvcnRFYXJseTogZmFsc2UgfSk7XHJcbiJdfQ==
--- build/schemas/WipeIntervalSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const joi_1 = require("joi");
-exports.default = joi_1.object({
-    value: joi_1.number()
-        .required(),
-}).options({ abortEarly: false });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiV2lwZUludGVydmFsU2NoZW1hLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL3NjaGVtYXMvV2lwZUludGVydmFsU2NoZW1hLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsNkJBQXFDO0FBRXJDLGtCQUFlLFlBQU0sQ0FBQztJQUNsQixLQUFLLEVBQUUsWUFBTSxFQUFFO1NBQ1YsUUFBUSxFQUFFO0NBQ2xCLENBQUMsQ0FBQyxPQUFPLENBQUMsRUFBRSxVQUFVLEVBQUUsS0FBSyxFQUFFLENBQUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IG51bWJlciwgb2JqZWN0IH0gZnJvbSAnam9pJztcclxuXHJcbmV4cG9ydCBkZWZhdWx0IG9iamVjdCh7XHJcbiAgICB2YWx1ZTogbnVtYmVyKClcclxuICAgICAgICAucmVxdWlyZWQoKSxcclxufSkub3B0aW9ucyh7IGFib3J0RWFybHk6IGZhbHNlIH0pO1xyXG4iXX0=
